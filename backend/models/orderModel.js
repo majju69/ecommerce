83202837480de3755ff18197fcaec4e13563c644
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-24 01:26:03 +0530

    004 Modeling Our Data

---
+++ backend/models/orderModel.js
@@ -0,0 +1,140 @@
+import mongoose from "mongoose";
+
+const orderSchema=mongoose.Schema(
+    {
+        user:
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "User"
+        },
+        orderItems:
+        [
+            { 
+                name:
+                {
+                    type: String, 
+                    required: true
+                },
+                qty:
+                {
+                    type: Number, 
+                    required: true,
+                    min: 0
+                },
+                image:
+                {
+                    type: String,
+                    required: true
+                },
+                price:
+                {
+                    type: Number, 
+                    required: true,
+                    min: 0
+                },
+                product:
+                {
+                    type: mongoose.Schema.Types.ObjectId,
+                    required: true,
+                    ref: "Product"
+                }
+            }
+        ],
+        shippingAddress:
+        {
+            address:
+            {
+                type: String,
+                required: true
+            },
+            city:
+            {
+                type: String,
+                required: true
+            },
+            postalCode:
+            {
+                type: String,
+                required: true
+            },
+            country:
+            {
+                type: String,
+                required: true
+            }
+        },
+        paymentMethod:
+        {
+            type: String,
+            required: true
+        },
+        paymentResult:
+        {
+            id:
+            {
+                type: String,
+            },
+            status:
+            {
+                type: String,
+            },
+            update_time:
+            {
+                type: String,
+            },
+            email_address:
+            {
+                type: String,
+            }
+        },
+        taxPrice:
+        {
+            type: Number,
+            required: true,
+            default:0.0,
+            min: 0
+        },
+        shippingPrice:
+        {
+            type: Number,
+            required: true,
+            default:0.0,
+            min: 0
+        },
+        totalPrice:
+        {
+            type: Number,
+            required: true,
+            min: 0,
+            default:0.0,
+        },
+        isPaid:
+        {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        paidAt:
+        {
+            type: Date
+        },
+        deliveredAt:
+        {
+            type: Date,
+        },
+        isDelivered:
+        {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    {
+        timestamps: true       // create the createdAt field
+    }
+);
+
+const Order=mongoose.model("Order", orderSchema);
+
+export default Order;
