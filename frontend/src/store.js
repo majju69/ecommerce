c2c7f7d5331711e3f0d2422a37b8c314bc766616
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-25 02:59:14 +0530

    003 Product List Reducer _ Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,17 @@
 import {legacy_createStore as createStore, combineReducers,applyMiddleware} from 'redux';       // createStore deprecated
 import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
+import { productListReducer } from './reducers/productReducers.js';
 
-const reducer = combineReducers({});
+const reducer = combineReducers(
+    {
+        productList:productListReducer,
+    });
 
 const initialState = {};
 
 const middleware = [thunk];
 
-const store =createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(reducer,initialState,composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
