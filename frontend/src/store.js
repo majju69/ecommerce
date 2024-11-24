1678e8ae1ec4fa1fd22f010a71622d75c436bbbd
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-25 03:31:12 +0530

    006 Product Details Reducer _ Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import {legacy_createStore as createStore, combineReducers,applyMiddleware} from 'redux';       // createStore deprecated
 import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
-import { productListReducer } from './reducers/productReducers.js';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers.js';
 
 const reducer = combineReducers(
     {
         productList:productListReducer,
+        productDetails:productDetailsReducer,
     });
 
 const initialState = {};
