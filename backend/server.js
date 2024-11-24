38da3b8db3d20a6e8fc624a36e94b724b9660d4b
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-24 17:44:15 +0530

    007 Fetching Products From The Database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,18 +10,9 @@ connectDB();
 
 const app=express();
 
+app.use("/api/products",productRoutes);
+
 const PORT=process.env.PORT || 8000;
-
-app.get("/api/products",(req, res) => 
-{
-    res.json(products);
-});
-
-app.get("/api/products/:id",(req, res) => 
-{
-    const product = products.find(product => product._id === req.params.id);
-    res.send(product);
-});
 
 app.listen(PORT,()=>
 {
