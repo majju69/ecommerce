5c13cc13c551706e2c98a7e4840c38c7fcf3f417
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-26 02:19:34 +0530

    003 Admin Screen Access Security

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS, USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS, USER_LIST_FAIL, USER_LIST_REQUEST, USER_LIST_RESET, USER_LIST_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS } from "../constants/userConstants";
 
 export const userLoginReducer = (state={},action) =>
 {
@@ -74,6 +74,8 @@ export const userListReducer = (state={users:[]},action) =>
             return {loading: false ,users: action.payload};
         case USER_LIST_FAIL:
             return {loading: false, error: action.payload};
+        case USER_LIST_RESET:
+            return {users: []};  // reset the user list when the user list is fetched  // This reducer handles the user list fetch, error, and reset actions.  // You can add more actions and reducers as needed for your application.  // For example, you might have a user order history reducer, user address reducer, etc.  // Be sure to follow best practices for handling user state and actions in Redux.  // For example, use Redux Toolkit or Ducks for managing state
         default:
             return state;
     }
