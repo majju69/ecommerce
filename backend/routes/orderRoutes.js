d618c12194c631d852d8a41a36fefbe54b3476fd
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-25 19:28:01 +0530

    001 Get Order by ID Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems,getOrderById } from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router=express.Router();
 
 router.route("/").post(protect,addOrderItems);
+router.route("/:id").get(protect,getOrderById);  // ensure that :id routes are at the bottom
 
 export default router;
