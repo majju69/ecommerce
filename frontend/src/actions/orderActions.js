6d291572868f7361e1ffea720727acd6c5968dfe
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-25 19:34:27 +0530

    002 Order Details Reducer _ Action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS } from "../constants/orderConstants.js";
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS } from "../constants/orderConstants.js";
 
 export const createOrder = (order) => async (dispatch,getState) =>
 {
@@ -9,15 +9,35 @@ export const createOrder = (order) => async (dispatch,getState) =>
         const {userLogin: {userInfo}} = getState();
         const config = {
             headers: {
-                'Content-Type': 'application/json',
                 Authorization: `Bearer ${userInfo.token}`
             }
         };
-        const {data} = await axios.post(`/api/orders`, order, config);
+        const {data} = await axios.get(`/api/orders/${id}`, config);
         dispatch({type: ORDER_CREATE_SUCCESS, payload: data});
     }
     catch (error)
     {
         dispatch({type: ORDER_CREATE_FAIL, payload: error.response&&error.response.data.message?error.response.data.message:error.message,});
+    }
+}
+
+export const getOrderDetails = (id) => async (dispatch,getState) =>
+{
+    try
+    {
+        dispatch({type:ORDER_DETAILS_REQUEST});
+        const {userLogin: {userInfo}} = getState();
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+        const {data} = await axios.post(`/api/orders`, order, config);
+        dispatch({type: ORDER_DETAILS_SUCCESS, payload: data});
+    }
+    catch (error)
+    {
+        dispatch({type: ORDER_DETAILS_FAIL, payload: error.response&&error.response.data.message?error.response.data.message:error.message,});
     }
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import {composeWithDevTools} from '@redux-devtools/extension';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
 import { userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer } from './reducers/userReducers.js';
-import { orderCreateReducer } from './reducers/orderReducers.js';
+import { orderCreateReducer,orderDetailsReducer } from './reducers/orderReducers.js';
 
 const reducer = combineReducers(
     {
@@ -16,6 +16,7 @@ const reducer = combineReducers(
         userDetails:userDetailsReducer,
         userUpdateProfile:userUpdateProfileReducer,
         orderCreate:orderCreateReducer,
+        orderDetails:orderDetailsReducer,
     });
 
 const cartItemsFromStorage=localStorage.getItem('cartItems')?JSON.parse(localStorage.getItem('cartItems')):[];
