877d17f760a527a2e9bc53844fa28341e515873f
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-26 17:57:22 +0530

    006 User Edit Screen _ Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen.js';
 import PlaceOrderScreen from './screens/PlaceOrderScreen.js';
 import OrderScreen from './screens/OrderScreen.js';
 import UserListScreen from './screens/UserListScreen.js';
+import UserEditScreen from './screens/UserEditScreen.js';
 
 const App=()=> {
   return (
@@ -31,6 +32,7 @@ const App=()=> {
             <Route path="/product/:id" component={ProductScreen}/>
             <Route path="/cart/:id?" component={CartScreen}/>
             <Route path="/admin/userlist" component={UserListScreen}/>
+            <Route path="/admin/user/:id/edit" component={UserEditScreen}/>
             <Route path="/" component={HomeScreen} exact/>
           </Container>
         </main>
