d3f933cec6888c2bc1c97a18bbc8a3361849c77d
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-27 01:51:40 +0530

    006 Admin Update Product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import {legacy_createStore as createStore, combineReducers,applyMiddleware} from 'redux';       // createStore deprecated
 import {thunk} from 'redux-thunk';
 import {composeWithDevTools} from '@redux-devtools/extension';
-import { productDetailsReducer, productListReducer,productDeleteReducer,productCreateReducer } from './reducers/productReducers.js';
+import { productDetailsReducer, productListReducer,productDeleteReducer,productCreateReducer,productUpdateReducer } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers.js';
 import { userLoginReducer,userRegisterReducer,userDetailsReducer,userUpdateProfileReducer,userListReducer,userDeleteReducer,userUpdateReducer } from './reducers/userReducers.js';
 import { orderCreateReducer,orderDetailsReducer,orderPayReducer,orderListMyReducer } from './reducers/orderReducers.js';
@@ -12,6 +12,7 @@ const reducer = combineReducers(
         productDetails:productDetailsReducer,
         productDelete:productDeleteReducer,
         productCreate:productCreateReducer,
+        productUpdate:productUpdateReducer,
         cart:cartReducer,
         userLogin:userLoginReducer, 
         userRegister:userRegisterReducer,
