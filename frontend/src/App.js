ce1d2eed884bbe36524c29c3a02d424a4c8dbccb
Kush Mazumdar <mazumdarkush2002@gmail.com>
2024-11-27 01:07:50 +0530

    005 Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,7 @@ import OrderScreen from './screens/OrderScreen.js';
 import UserListScreen from './screens/UserListScreen.js';
 import UserEditScreen from './screens/UserEditScreen.js';
 import ProductListScreen from './screens/ProductListScreen.js';
+import ProductEditScreen from './screens/ProductEditScreen.js';
 
 const App=()=> {
   return (
@@ -35,6 +36,7 @@ const App=()=> {
             <Route path="/admin/userlist" component={UserListScreen}/>
             <Route path="/admin/user/:id/edit" component={UserEditScreen}/>
             <Route path="/admin/productlist" component={ProductListScreen}/>
+            <Route path="/admin/product/:id/edit" component={ProductEditScreen}/>
             <Route path="/" component={HomeScreen} exact/>
           </Container>
         </main>
